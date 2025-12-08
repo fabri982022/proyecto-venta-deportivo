1082553dd3dc5760f9b20e3bd44ab322ef623f42
fabri98bazan <fabri98201098@gmail.com>
2025-12-08 10:42:49 -0300

    contruccion de la base. Back, front y el docker-compose.

---
+++ back/src/test/java/com/peoyecto/venta/deportiva/deporte/DeporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peoyecto.venta.deportiva.deporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
